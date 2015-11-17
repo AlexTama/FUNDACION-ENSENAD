04cd5fb5c5718969e4e028066565ad162541e5c2
Alexander <alextamayop@gmail.com>
2015-11-17 11:01:38 -0500

    pagina finalizada

---
+++ ERROR.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<!-- META TAGS AND CSS STYLE FOR THE WEB SITE -->
+	<?php include 'includes/headers.php'; ?>
+
+	<title>Contacto | Fundación Id y Enseñad</title>
+</head>
+<body>
+
+	<!-- *************************************** -->
+	<!-- FIRST SECTION -->
+	<!-- menu -->
+	<!-- *************************************** -->
+	<section class="container-fluid">
+		<div class="row">
+			<div class="col-md-12" style="padding:12% 0 0;">
+				
+				<!-- MENU -->
+				<?php include 'includes/menu.php'; ?>
+
+				<h1 class="text-center" style="color:#000000; letter-spacing: 5px;"><b>CONTÁCTENOS</b></h1>
+			</div>
+		</div>
+	</section>
+	
+	<!-- *************************************** -->
+	<!-- SECOND SECTION -->
+	<!-- contact form -->
+	<!-- *************************************** -->
+
+	<section class="container-fluid">
+		<div class="row" style="padding-bottom: 5%;">
+			<div class="col-md-offset-4 col-md-4">
+
+				<form action="envia.php" method="POST">
+					<div class="form-group">
+						<input type="text" name="nombre" class="form-control input-style-reset" placeholder="NOMBRE">
+					</div>
+				  	<div class="form-group">				    	
+				    	<input type="text" name="asunto" class="form-control input-style-reset" placeholder="ASUNTO">
+				  	</div>
+				  	<div class="form-group">
+					    <input type="email" class="form-control input-style-reset" name="email" placeholder="CORREO" require>
+					 </div>
+					 <textarea class="form-control input-style-reset" rows="6" name="mensaje" placeholder="MENSAJE" style="resize:none;" ></textarea>
+					
+					<div style="padding: 20px" class="text-center">
+						<div class="g-recaptcha" data-sitekey="6LeQJBETAAAAAFlCxez7X3kn1EW9W8dCVqcEg_ZJ"></div>
+					</div>
+					<div class="form-group text-center" style="color:#1700AC;">
+						<p class="lead">¡COMPRUEBE QUE NO ES UN ROBOT!</p>
+					</div>
+						
+					<div class="text-center">
+						<input class="ghost-button-profile" type="submit" name="enviar" value="ENVIAR" style="letter-spacing:5px; font-weight:bold;">
+					</div>
+				</form>
+
+			</div>
+		</div>
+	</section>
+
+	<!-- ***************************************************************** -->
+	<!-- FOOTER -->
+	<!-- ***************************************************************** -->
+	<?php include 'includes/footer.php'; ?>
+	
+	<!-- SCRIPTS FOR ANIMATION -->
+	<?php include 'includes/scripts.php'; ?>
+</body>
+</html>
--- envia.php
+++ envia.php
@@ -1,57 +1,59 @@
 <?php
 //comprueba que exista la petición y no está vacío el arreglo
 
-if (isset($_POST["g-recaptcha-response"]) && $_POST["g-recaptcha-response"]) {
+if (isset($_POST["g-recaptcha-response"]) && $_POST["g-recaptcha-response"] && $_POST["enviar"]) 
+{
 
 	/**************************************************************************/
 	/* CONTROL DE SPAM CON EL SISTEMA DE RECAPTCHA */
 	/**************************************************************************/
 
 	$secret_key = "6LeQJBETAAAAAJbzkU2zmjUSNvjh7CnKLtZkHynm";
-	$ip 		= $_SERVER["REMOTE_ADDR"];			//ip del usuario de la aplicación
+	//$ip 		= $_SERVER["REMOTE_ADDR"];			//ip del usuario de la aplicación &remoteid=$ip
 	$captcha	= $_POST["g-recaptcha-response"];	//captura los datos de validación
-	$result		= file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret_key&response=$captcha&remoteid=$ip");
+	$result		= file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret_key&response=$captcha");
 
-	if ($result) {
+	/**************************************************************************/
+	/* CAPTURA DE DATOS DEL USUARIO PARA EL ENVÍO DEL FORMULARIO */
+	/**************************************************************************/
 
-		if (isset($_POST["enviar"])) {
+	@$nombre 	= $_POST["nombre"];
+	@$asunto 	= $_POST["asunto"];
+	@$email 		= $_POST["email"];
+	@$mensaje 	= $_POST["mensaje"];
+				
+	/**************************************************************************/
+	/* ENVÍO DE DATOS AL CORREO */
+	/**************************************************************************/
 
-			/**************************************************************************/
-			/* CAPTURA DE DATOS DEL USUARIO PARA EL ENVÍO DEL FORMULARIO */
-			/**************************************************************************/
+	$to 		= "alextamayop@gmail.com";
+	$subject 	= $asunto;
+	$message	= $mensaje;
+	$headers	= "From: ".$nombre." ".$email;
 
-			@$nombre 	= $_POST["nombre"];
-			@$asunto 	= $_POST["asunto"];
-			@$email 		= $_POST["email"];
-			@$mensaje 	= $_POST["mensaje"];
-			
-			/**************************************************************************/
-			/* ENVÍO DE DATOS AL CORREO */
-			/**************************************************************************/
-
-			$to 		= "alextamayop@gmail.com";
-			$subject 	= $asunto;
-			$message	= $mensaje;
-			$headers	= "From: ".$nombre." ".$email;
-
-			
-			include 'gracias.php' ;
-
-			if (@mail($to, $subject, $message, $headers)) {
-				include '../gracias.php';
-			}
-
-			else {
-
-			}
-
+	if (!($result)) 
+	{
+		if (@mail($to, $subject, $message, $headers))
+		{
+			include 'gracias.php';
 		}
 
-	} 
-
-	else {
-		echo "falló envío";
+		else 
+		{
+			echo "no envio correo por no estar en servudor remto";
+		}
 	}
+
+	else
+	{
+		echo "Puedo enviar el correo";
+	}
+
+}
+
+else 
+{
+	include 'ERROR.php';
 }
 
  ?>
